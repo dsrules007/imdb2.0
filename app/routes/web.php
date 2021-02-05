8e11776d3ccc30fa1cfb65805f4f8847c6a9f424
dsrules007 <sousilvaney@gmail.com>
2021-02-05 08:53:02 +0000

    add routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// create
+Route::post('/insertUser','userController@create');
+Route::post('/createMovie','movieController@create');
+Route::post('/createReview','reviewController@create');
+
+//delete
+Route::post('/deleteUser','userController@delete');
+Route::post('/deleteMovie','movieController@delete');
+Route::post('/deleteReview','reviewController@delete');
+
+// get and get all
+Route::get('/getUser','userController@get');
+Route::get('/listUsers','userController@list');
+
+Route::get('/getMovie','movieController@get');
+Route::get('/listMovie','movieController@list');
+
+Route::get('/getReview','reviewController@get');
+Route::get('/listReview','reviewController@list');
+
+//Get Average rating of movie
+
+//Get Average rating of user
